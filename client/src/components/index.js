a417b1d0fbe9bc645a66ba789842580512f88947
robonexx <robertwagar@gmail.com>
2023-04-23 18:01:20 +0200

    all components added

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomBtn from './buttons/CustomBtn';
+import AIPicker from './AIPicker'
+import ColorPicker from './ColorPicker'
+import FilePicker from './FilePicker'
+import Tab from './Tab'
+
+export { CustomBtn, AIPicker, ColorPicker, FilePicker, Tab };
